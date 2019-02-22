7f5258cb4744600cf2606f9ec7b6444fb7cfd5f1
Evgeny Belkin <belkin@informatica.ru>
2019-02-22 16:39:13 +0300

    данные по коммиту при первом запросе сохраняются в базу, потом берутся из нее

--- models/GithubForm.php
+++ models/GithubForm.php
@@ -3,8 +3,9 @@
 namespace app\models;
 
 use Yii;
-use yii\base\Model;
+use app\models\Commit;
 use linslin\yii2\curl;
+use yii\base\Model;
 
 class GithubForm extends Model
 {
@@ -68,44 +69,68 @@ class GithubForm extends Model
             return null;
         }
         $result = [];
+        $commit = null;
         foreach ($commits as $c) {
-            $curl = new curl\Curl();
-            $response = $curl->setHeaders([
-                'Authorization' => 'token ' . Yii::$app->params['github_token']
-            ])->get('https://api.github.com/repos/' . $username . '/' . $reponame . '/commits/' . $c['sha']);
-            if ($curl->errorCode === null) {
-                $raw_result = json_decode($response);
-                if ($raw_result && property_exists($raw_result, 'files')) {
-                    foreach ($raw_result->files as $r) {
-                        $id = $r->filename;
-                        if (!isset($result[$id])) {
-                            $result[$id] = [
-                                'filename' => $r->filename,
-                                'commits' => 1,
-                                'authors' => [
-                                    $c['author_email'] => [
-                                        'name' => $c['author'],
-                                        'email' => $c['author_email'],
-                                        'count' => 1
-                                    ]
-                                ]
-                            ];                        
-                        } else {
-                            $result[$id]['commits'] += 1;
-                            if (!isset($result[$id]['authors'][$c['author_email']])) {
-                                $result[$id]['authors'][$c['author_email']] = [
+            $commit = Commit::find()->where(['sha' => $c['sha']])->one();
+            $files = [];
+            if ($commit === NULL) {
+                $curl = new curl\Curl();
+                $response = $curl->setHeaders([
+                    'Authorization' => 'token ' . Yii::$app->params['github_token']
+                ])->get('https://api.github.com/repos/' . $username . '/' . $reponame . '/commits/' . $c['sha']);
+                if ($curl->errorCode === null) {
+                    $commit = json_decode($response);
+                    if ($commit && property_exists($commit, 'files')) {
+                        foreach ($commit->files as $f) {
+                            $files[] = [
+                                'filename' => $f->filename
+                            ];
+                        }
+                    }
+                    $newCommit = new Commit();
+                    $newCommit->sha = $c['sha'];
+                    $newCommit->files = json_encode($files);
+                    $newCommit->save();
+                } else {
+                    // TODO Error Handling $curl->errorCode
+                }
+            } else {
+                $tmp_files = json_decode($commit->files);
+                foreach ($tmp_files as $f) {
+                    $files[] = [
+                        'filename' => $f->filename
+                    ];
+                }
+            }
+            if ($commit && is_array($files) && !empty($files)) {
+                foreach ($files as $r) {
+                    $id = $r['filename'];
+                    $files[] = $r['filename'];
+                    if (!isset($result[$id])) {
+                        $result[$id] = [
+                            'filename' => $r['filename'],
+                            'commits' => 1,
+                            'authors' => [
+                                $c['author_email'] => [
                                     'name' => $c['author'],
                                     'email' => $c['author_email'],
                                     'count' => 1
-                                ];
-                            } else {
-                                $result[$id]['authors'][$c['author_email']]['count'] += 1;
-                            }
+                                ]
+                            ]
+                        ];                        
+                    } else {
+                        $result[$id]['commits'] += 1;
+                        if (!isset($result[$id]['authors'][$c['author_email']])) {
+                            $result[$id]['authors'][$c['author_email']] = [
+                                'name' => $c['author'],
+                                'email' => $c['author_email'],
+                                'count' => 1
+                            ];
+                        } else {
+                            $result[$id]['authors'][$c['author_email']]['count'] += 1;
                         }
                     }
                 }
-            } else {
-                // TODO Error Handling $curl->errorCode
             }
         }
         $preparedResult = [];
